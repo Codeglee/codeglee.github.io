b5638a880414ccaf41e7497e1b13e9170cde5a17
Tristan Warner-Smith <Codeglee@users.noreply.github.com>
2022-03-23 16:21:49 +0000

    Adds UI testing post

---
+++ UITestSnippets.playground/Contents.swift
@@ -0,0 +1,152 @@
+import SwiftUI
+
+//
+//  UITestSnippests.swift
+//
+//
+//  Created by Tristan Warner-Smith on 23/03/2022.
+//
+
+import SwiftUI
+import Foundation
+
+//final class OnboardingViewModel: ObservableObject {
+//    @Published var showOnboarding: Bool
+//
+//    init(showOnboarding: Bool = UserDefaults.standard.bool(forKey: "hasOnboardingBeenShown")) {
+//        self.showOnboarding = showOnboarding
+//    }
+//
+//    func markOnboardingAsShown() {
+//        UserDefaults.standard.set(true, forKey: "hasOnboardingBeenShown")
+//        showOnboarding = false
+//    }
+//}
+
+// NOTE: You can swap actor for a final class with a standard singleton
+final class AutomationContext {
+    static let shared = AutomationContext()
+
+    private let settingStore: SettingStorage
+
+    var showOnboarding: Bool {
+        didSet {
+            settingStore.showOnboarding
+        }
+    }
+
+    private init(settingStore: SettingStorage = SettingStore()) {
+        self.settingStore = settingStore
+
+        showOnboarding = settingStore.showOnboarding
+    }
+}
+
+protocol SettingStorage {
+    var showOnboarding: Bool { get set }
+}
+
+struct SettingStore: SettingStorage {
+    var showOnboarding: Bool {
+        get {
+            UserDefaults.standard.bool(forKey: "hasOnboardingBeenShown")
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: "hasOnboardingBeenShown")
+        }
+    }
+}
+
+final class AppViewModel: ObservableObject {
+    @Published private(set) var showOnboarding: Bool
+    private var settingStore: SettingStorage
+
+    init(settingStore: SettingStorage = SettingStore()) {
+        self.settingStore = settingStore
+        showOnboarding = settingStore.showOnboarding
+    }
+
+    func markOnboardingSeen() {
+        settingStore.showOnboarding = false
+        showOnboarding = false
+    }
+}
+
+struct MyApp: App {
+
+    @StateObject var app = AppViewModel()
+
+    var body: some Scene {
+        WindowGroup {
+            if app.showOnboarding {
+                OnboardingView(
+                    complete: {
+                        app.markOnboardingSeen()
+                    }
+                )
+            } else {
+                MainAppFlowView()
+            }
+        }
+    }
+}
+
+struct OnboardingView: View {
+    let complete: () -> Void
+
+    var body: some View {
+        VStack {
+            Text("Imagine a lengthy onboarding flow here")
+
+            Button(
+                action: complete,
+                label: {
+                    Text("Okay")
+                }
+            )
+        }
+    }
+}
+
+struct MainAppFlowView: View {
+    var body: some View {
+        Text("Our main app flow")
+    }
+}
+
+
+//@main
+enum AppLauncher {
+
+    static func main() throws {
+
+        LaunchArgumentConfigurator.configure(
+            AutomationContext.shared,
+            with: CommandLine.arguments
+        )
+
+        MyApp.main()
+    }
+}
+
+enum LaunchArgumentConfigurator {
+
+    static func configure(
+        _ context: AutomationContext,
+        with launchArguments: [String]
+    ) {
+        if launchArguments.contains("-skipOnboarding") {
+            context.showOnboarding = false
+        }
+    }
+}
+
+import XCTest
+
+func testSkipsOnboarding() {
+    let app = XCUIApplication()
+    app.launchArguments.append("-skipOnboarding")
+    app.launch()
+
+    XCTFail("We're not actually verifying we've skipped onboarding yet")
+}
